a0d89ff65a02697910627b7251a2db945b7f064e
Aldair <114087399+aldairmendes@users.noreply.github.com>
2025-08-21 11:49:24 -0300

    Add files via upload

---
+++ Drum Kit/index.js
@@ -0,0 +1,56 @@
+let btns = document.querySelectorAll(".drum");
+
+const makeSound = (key) => {
+    switch (key) {
+      case 'w':
+        let tom1 = new Audio('./sounds/tom-1.mp3');
+        tom1.play();
+        break;
+      case 'a':
+        let tom2 = new Audio('./sounds/tom-2.mp3');
+        tom2.play();
+        break;
+      case 's':
+        let tom3 = new Audio('./sounds/tom-3.mp3');
+        tom3.play();
+        break;
+      case 'd':
+        let tom4 = new Audio('./sounds/tom-4.mp3');
+        tom4.play();
+        break;
+      case 'j':
+        let snare = new Audio('./sounds/snare.mp3');
+        snare.play();
+        break;
+      case 'k':
+        let crash = new Audio('./sounds/crash.mp3');
+        crash.play();
+        break;
+      case 'l':
+        let kick = new Audio('./sounds/kick-bass.mp3');
+        kick.play();
+        break;
+      default:
+        console.log(key);
+    }
+};
+
+const buttonAnimation = (btn) => {
+  document.querySelector("." + btn).classList.add("pressed");
+  setTimeout(() => {document.querySelector("." + btn).classList.remove("pressed")}, 100);
+}
+
+//Detect key pressed
+document.addEventListener("keydown", (event) => {
+  makeSound(event.key);
+  buttonAnimation(event.key)
+});
+
+//Detect mouse click
+for (let i=0; i<btns.length; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    let btnInnerHtml = this.innerHTML;
+    makeSound(btnInnerHtml);
+    buttonAnimation(btnInnerHtml)
+  })
+  }
